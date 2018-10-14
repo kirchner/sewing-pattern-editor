2a775d73bb07e2ec08a6aa6aeafac75ddcdd8e74
Fabian Kirchner <kirchner@posteo.de>
2018-10-14 17:41:56 +0200

    add license

--- src/Store.elm
+++ src/Store.elm
@@ -1,15 +1,32 @@
-module Store
-    exposing
-        ( Entry
-        , Store
-        , decoder
-        , empty
-        , encode
-        , get
-        , insert
-        , toList
-        , values
-        )
+module Store exposing
+    ( Entry
+    , Store
+    , decoder
+    , empty
+    , encode
+    , get
+    , insert
+    , toList
+    , values
+    )
+
+{-
+   Sewing pattern editor
+   Copyright (C) 2018  Fabian Kirchner <kirchner@posteo.de>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
 
 import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
