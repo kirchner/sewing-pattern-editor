9e27484e19bdebc706efaeaa1aed8bf111b832df
Fabian Kirchner <kirchner@posteo.de>
2019-05-05 14:55:53 +0200

    fix routing in server

--- server.go
+++ server.go
@@ -22,11 +22,18 @@ func main() {
 
 	r := mux.NewRouter()
 
+	r.Methods("GET").PathPrefix("/patterns").Handler(gziphandler.GzipHandler(
+		serveIndex(distPath)))
+	r.Methods("GET").PathPrefix("/measurements").Handler(gziphandler.GzipHandler(
+		serveIndex(distPath)))
+	r.Methods("GET").PathPrefix("/persons").Handler(gziphandler.GzipHandler(
+		serveIndex(distPath)))
 	r.Methods("GET").PathPrefix("/editor").Handler(gziphandler.GzipHandler(
 		serveIndex(distPath)))
 
 	r.Methods("GET").PathPrefix("/").Handler(gziphandler.GzipHandler(
 		http.FileServer(http.Dir(distPath))))
+
 	http.Handle("/", r)
 
 	log.Println("Listening on port " + port)
