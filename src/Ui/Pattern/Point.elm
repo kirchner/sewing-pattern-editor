9de8649b15be66f399315aac56fcdf8c54897b80
Fabian Kirchner <kirchner@posteo.de>
2019-11-02 14:28:33 +0100

    refactor: clean up code a bit

--- src/Ui/Pattern/Point.elm
+++ src/Ui/Pattern/Point.elm
@@ -39,242 +39,190 @@ type Info coordinates
         }
 
 
-draw : Quantity Float (Rate Pixels Meters) -> Config coordinates -> Svg msg
+type alias Resolution =
+    Quantity Float (Rate Pixels Meters)
+
+
+draw : Resolution -> Config coordinates -> Svg msg
 draw resolution cfg =
     let
-        labelPosition =
-            point
-                |> Point2d.translateBy (Vector2d.pixels 17 -13)
-                |> Point2d.toPixels
-
         point =
             Point2d.at resolution cfg.point
     in
     Svg.g []
         [ if cfg.focused then
-            Svg.circle2d
-                [ Svg.Attributes.fill "none"
-                , Svg.Attributes.stroke (toColor Ui.Color.primary)
-                , Svg.Attributes.strokeWidth "2"
-                , Svg.Attributes.strokeDasharray "4 6"
-                , Svg.Attributes.strokeLinecap "round"
-                ]
-                (Circle2d.withRadius (pixels 8) point)
+            focusOutline point
 
           else
             Svg.text ""
         , if cfg.focused || cfg.hovered then
-            Svg.text_
-                [ Svg.Attributes.x (String.fromFloat labelPosition.x)
-                , Svg.Attributes.y (String.fromFloat labelPosition.y)
-                , Svg.Attributes.textAnchor "middle"
-                , Svg.Attributes.style <|
-                    "font-size: "
-                        ++ String.fromFloat 14
-                        ++ "px; font-family: \"Rubik\";"
-                , Svg.Attributes.fill (toColor Ui.Color.primary)
-                ]
-                [ Svg.text cfg.label ]
+            pointLabel point cfg.label
 
           else
             Svg.text ""
-        , if cfg.focused then
-            case cfg.info of
-                Origin ->
-                    Svg.text ""
-
-                FromOnePoint info ->
-                    let
-                        basePoint =
-                            Point2d.at resolution info.basePoint
-
-                        offsetBasePoint =
-                            case Direction2d.from basePoint point of
-                                Nothing ->
-                                    basePoint
-
-                                Just direction ->
-                                    basePoint
-                                        |> Point2d.translateBy (Vector2d.withLength (pixels 3) direction)
-
-                        offsetPoint =
-                            case Direction2d.from point basePoint of
-                                Nothing ->
-                                    point
-
-                                Just direction ->
-                                    point
-                                        |> Point2d.translateBy (Vector2d.withLength (pixels 8.5) direction)
-
-                        length =
-                            Vector2d.from offsetPoint offsetBasePoint
-                                |> Vector2d.length
-                                |> Pixels.inPixels
-
-                        distancePosition =
-                            basePoint
-                                |> Point2d.translateBy
-                                    (Vector2d.from basePoint point
-                                        |> Vector2d.scaleBy 0.5
-                                    )
-                                |> Point2d.toPixels
-
-                        distanceTransforms attrs =
-                            Vector2d.from basePoint point
-                                |> Vector2d.direction
-                                |> Maybe.map (Direction2d.toAngle >> Angle.inDegrees)
-                                |> Maybe.map
-                                    (\angle ->
-                                        Svg.Attributes.transform
-                                            (String.concat
-                                                [ "rotate("
-                                                , String.fromFloat angle
-                                                , " "
-                                                , String.fromFloat distancePosition.x
-                                                , " "
-                                                , String.fromFloat distancePosition.y
-                                                , ")"
-                                                ]
-                                            )
-                                            :: attrs
-                                    )
-                                |> Maybe.withDefault attrs
-                    in
-                    Svg.g []
-                        [ Svg.circle2d
-                            [ Svg.Attributes.fill (toColor Ui.Color.primary) ]
-                            (Circle2d.withRadius (pixels 3) basePoint)
-                        , Svg.lineSegment2d
-                            [ Svg.Attributes.stroke (toColor Ui.Color.primary)
-                            , Svg.Attributes.strokeWidth "1.5"
-                            , strokeDasharray length 8 10
-                            , Svg.Attributes.strokeLinecap "round"
-                            ]
-                            (LineSegment2d.from offsetPoint offsetBasePoint)
-                        , Svg.text_
-                            ([ Svg.Attributes.x (String.fromFloat distancePosition.x)
-                             , Svg.Attributes.y (String.fromFloat distancePosition.y)
-                             , Svg.Attributes.dy (String.fromFloat -10)
-                             , Svg.Attributes.textAnchor "middle"
-                             , Svg.Attributes.style <|
-                                "font-size: "
-                                    ++ String.fromFloat 14
-                                    ++ "px; font-family: \"Rubik\";"
-                             , Svg.Attributes.fill (toColor Ui.Color.primary)
-                             ]
-                                |> distanceTransforms
-                            )
-                            [ Svg.text info.distance ]
-                        ]
-
-          else if cfg.hovered then
-            case cfg.info of
-                Origin ->
-                    Svg.text ""
-
-                FromOnePoint info ->
-                    let
-                        basePoint =
-                            Point2d.at resolution info.basePoint
-
-                        offsetBasePoint =
-                            case Direction2d.from basePoint point of
-                                Nothing ->
-                                    basePoint
-
-                                Just direction ->
-                                    basePoint
-                                        |> Point2d.translateBy (Vector2d.withLength (pixels 3) direction)
-
-                        offsetPoint =
-                            case Direction2d.from point basePoint of
-                                Nothing ->
-                                    point
-
-                                Just direction ->
-                                    point
-                                        |> Point2d.translateBy (Vector2d.withLength (pixels 5) direction)
-
-                        length =
-                            Vector2d.from offsetPoint offsetBasePoint
-                                |> Vector2d.length
-                                |> Pixels.inPixels
-
-                        distancePosition =
-                            basePoint
-                                |> Point2d.translateBy
-                                    (Vector2d.from basePoint point
-                                        |> Vector2d.scaleBy 0.5
-                                    )
-                                |> Point2d.toPixels
-
-                        distanceTransforms attrs =
-                            Vector2d.from basePoint point
-                                |> Vector2d.direction
-                                |> Maybe.map (Direction2d.toAngle >> Angle.inDegrees)
-                                |> Maybe.map
-                                    (\angle ->
-                                        Svg.Attributes.transform
-                                            (String.concat
-                                                [ "rotate("
-                                                , String.fromFloat angle
-                                                , " "
-                                                , String.fromFloat distancePosition.x
-                                                , " "
-                                                , String.fromFloat distancePosition.y
-                                                , ")"
-                                                ]
-                                            )
-                                            :: attrs
-                                    )
-                                |> Maybe.withDefault attrs
-                    in
-                    Svg.g []
-                        [ Svg.circle2d
-                            [ Svg.Attributes.fill (toColor Ui.Color.primary) ]
-                            (Circle2d.withRadius (pixels 3) basePoint)
-                        , Svg.lineSegment2d
-                            [ Svg.Attributes.stroke (toColor Ui.Color.primary)
-                            , Svg.Attributes.strokeWidth "1"
-                            , strokeDasharray length 8 10
-                            , Svg.Attributes.strokeLinecap "round"
-                            ]
-                            (LineSegment2d.from offsetPoint offsetBasePoint)
-                        , Svg.text_
-                            ([ Svg.Attributes.x (String.fromFloat distancePosition.x)
-                             , Svg.Attributes.y (String.fromFloat distancePosition.y)
-                             , Svg.Attributes.dy (String.fromFloat -10)
-                             , Svg.Attributes.textAnchor "middle"
-                             , Svg.Attributes.style <|
-                                "font-size: "
-                                    ++ String.fromFloat 14
-                                    ++ "px; font-family: \"Rubik\";"
-                             , Svg.Attributes.fill (toColor Ui.Color.primary)
-                             ]
-                                |> distanceTransforms
-                            )
-                            [ Svg.text info.distance ]
-                        ]
+        , if cfg.focused || cfg.hovered then
+            pointInfo resolution cfg
 
           else
             Svg.text ""
-        , Svg.circle2d
-            [ Svg.Attributes.fill <|
-                if cfg.hovered then
-                    toColor Ui.Color.primary
-
-                else
-                    "none"
-            , Svg.Attributes.stroke <|
-                if cfg.hovered then
-                    toColor Ui.Color.primary
-
-                else
-                    toColor Ui.Color.black
-            , Svg.Attributes.strokeWidth "1"
-            ]
-            (Circle2d.withRadius (pixels 5) point)
+        , actualPoint cfg.hovered point
         ]
+
+
+focusOutline : Point2d Pixels coordinates -> Svg msg
+focusOutline point =
+    Svg.circle2d
+        [ Svg.Attributes.fill "none"
+        , Svg.Attributes.stroke (toColor Ui.Color.primary)
+        , Svg.Attributes.strokeWidth "2"
+        , Svg.Attributes.strokeDasharray "4 6"
+        , Svg.Attributes.strokeLinecap "round"
+        ]
+        (Circle2d.withRadius (pixels 8) point)
+
+
+pointLabel : Point2d Pixels coordinates -> String -> Svg msg
+pointLabel point label =
+    let
+        labelPosition =
+            point
+                |> Point2d.translateBy (Vector2d.pixels 17 -13)
+                |> Point2d.toPixels
+    in
+    Svg.text_
+        [ Svg.Attributes.x (String.fromFloat labelPosition.x)
+        , Svg.Attributes.y (String.fromFloat labelPosition.y)
+        , Svg.Attributes.textAnchor "middle"
+        , font
+        , Svg.Attributes.fill (toColor Ui.Color.primary)
+        ]
+        [ Svg.text label ]
+
+
+pointInfo : Resolution -> Config coordinates -> Svg msg
+pointInfo resolution cfg =
+    let
+        point =
+            Point2d.at resolution cfg.point
+    in
+    case cfg.info of
+        Origin ->
+            Svg.text ""
+
+        FromOnePoint info ->
+            let
+                basePoint =
+                    Point2d.at resolution info.basePoint
+
+                offsetBasePoint =
+                    case Direction2d.from basePoint point of
+                        Nothing ->
+                            basePoint
+
+                        Just direction ->
+                            basePoint
+                                |> Point2d.translateBy (Vector2d.withLength (pixels 3) direction)
+
+                offsetPoint =
+                    case Direction2d.from point basePoint of
+                        Nothing ->
+                            point
+
+                        Just direction ->
+                            point
+                                |> Point2d.translateBy
+                                    (Vector2d.withLength (pixels offsetPointFactor) direction)
+
+                offsetPointFactor =
+                    if cfg.focused then
+                        8.5
+
+                    else
+                        5
+
+                length =
+                    Vector2d.from offsetPoint offsetBasePoint
+                        |> Vector2d.length
+                        |> Pixels.inPixels
+            in
+            Svg.g []
+                [ Svg.circle2d
+                    [ Svg.Attributes.fill (toColor Ui.Color.primary) ]
+                    (Circle2d.withRadius (pixels 3) basePoint)
+                , Svg.lineSegment2d
+                    [ Svg.Attributes.stroke (toColor Ui.Color.primary)
+                    , Svg.Attributes.strokeWidth "1.5"
+                    , strokeDasharray length 8 10
+                    , Svg.Attributes.strokeLinecap "round"
+                    ]
+                    (LineSegment2d.from offsetPoint offsetBasePoint)
+                , lineLabel basePoint point info.distance
+                ]
+
+
+lineLabel : Point2d Pixels coordinates -> Point2d Pixels coordinates -> String -> Svg msg
+lineLabel basePoint point label =
+    let
+        position =
+            basePoint
+                |> Point2d.translateBy (Vector2d.from basePoint point |> Vector2d.scaleBy 0.5)
+                |> Point2d.toPixels
+
+        addTransform attrs =
+            Vector2d.from basePoint point
+                |> Vector2d.direction
+                |> Maybe.map (Direction2d.toAngle >> Angle.inDegrees >> addRotateTransform attrs)
+                |> Maybe.withDefault attrs
+
+        addRotateTransform attrs angle =
+            Svg.Attributes.transform
+                (String.concat
+                    [ "rotate("
+                    , String.fromFloat angle
+                    , " "
+                    , String.fromFloat position.x
+                    , " "
+                    , String.fromFloat position.y
+                    , ")"
+                    ]
+                )
+                :: attrs
+    in
+    Svg.text_
+        ([ Svg.Attributes.x (String.fromFloat position.x)
+         , Svg.Attributes.y (String.fromFloat position.y)
+         , Svg.Attributes.dy (String.fromFloat -10)
+         , Svg.Attributes.textAnchor "middle"
+         , font
+         , Svg.Attributes.fill (toColor Ui.Color.primary)
+         ]
+            |> addTransform
+        )
+        [ Svg.text label ]
+
+
+actualPoint : Bool -> Point2d Pixels coordinates -> Svg msg
+actualPoint hovered point =
+    Svg.circle2d
+        [ Svg.Attributes.fill <|
+            if hovered then
+                toColor Ui.Color.primary
+
+            else
+                "none"
+        , Svg.Attributes.stroke <|
+            if hovered then
+                toColor Ui.Color.primary
+
+            else
+                toColor Ui.Color.black
+        , Svg.Attributes.strokeWidth "1"
+        ]
+        (Circle2d.withRadius (pixels 5) point)
+
+
+
+---- HELPER
 
 
 toColor : Color -> String
@@ -310,3 +258,11 @@ strokeDasharray length strokeLength strokeGap =
             floor (length - toFloat strokeLength) // (strokeLength + strokeGap)
     in
     Svg.Attributes.strokeDasharray ("8 " ++ String.fromFloat actualStrokeGap)
+
+
+font : Attribute msg
+font =
+    Svg.Attributes.style <|
+        "font-size: "
+            ++ String.fromFloat 14
+            ++ "px; font-family: \"Rubik\";"
