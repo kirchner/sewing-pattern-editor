7c3a5bb8475c80cc7af2ca1fe5bcc33b44ecb6fd
Fabian Kirchner <kirchner@posteo.de>
2019-12-22 13:03:55 +0100

    remove pre-aching again

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,6 @@
 // Files to cache
 var cacheName = 'sewing-pattern-editor--v1';
-var appShellFiles = [
-  './index.html',
-  './assets/icon-32.png',
-  './assets/icon-64.png',
-  './assets/icon-128.png',
-  './assets/icon-256.png',
-  './assets/icon-512.png',
-  './assets/rubik-v9-latin-300.woff',
-  './assets/rubik-v9-latin-300.woff2',
-  './assets/mansalva-v1-latin-regular.woff',
-  './assets/mansalva-v1-latin-regular.woff2',
-  './main.css',
-  './index.js',
-  './manifest.webmanifest',
-  '@fortawesome/fontawesome-free/css/all.css'
-];
+var appShellFiles = [];
 
 // Installing Service Worker
 self.addEventListener('install', function(e) {
